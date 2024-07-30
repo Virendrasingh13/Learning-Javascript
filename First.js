a57d7d49c179099d9fe7fe6cbcb9176c8c212d1e
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2024-07-30 23:45:11 +0530

    Started with JavaScript - Concepts Cleared (Variables, Inline function, For of, For each)

---
+++ First.js
@@ -0,0 +1,50 @@
+// function noOfVovel(str){
+//     let count =0;
+//     for(let a of str)
+//     {
+//         if(a==="a" || a === "e" || a==="o" || a==="u" || a==="i")
+//         {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+// noOfVovel("vovel")
+
+
+// const countVovel = (str) =>{
+//     count =0;
+//     for(let a of str){
+//         if(a==="a" || a==="e" || a==="i" || a==="o" || a==="u")
+//         {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+// console.log(countVovel);
+
+
+// const sumOfNumber = (n)=>{
+//     let sum=0;
+//     for(let i=n;i>=0;i--){
+//         sum+=i;
+//     }
+//     console.log(sum);
+// }
+
+// sumOfNumber(5)
+
+
+// const sumOfNum = (n)=>{
+//     let i=n;
+//     let count=0;
+//         while(i>=0)
+//         {
+//             count+=i;
+//             i--;
+//         }
+//         console.log(count);
+// }
+// sumOfNum(5)
