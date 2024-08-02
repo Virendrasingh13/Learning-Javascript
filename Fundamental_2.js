aad7680529c82cfb34927bfab68859a2696a78d6
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2024-08-02 16:00:47 +0000

    Timeout Function

---
+++ Fundamental_2.js
@@ -0,0 +1,15 @@
+//Asyncronious Programming
+
+
+// 01.Settimeout Function
+
+// function getdata(data){
+//     setTimeout( () => {console.log("Data : ",data);
+//     } ,2000)
+// }
+
+// const fun = setTimeout( () =>{
+//     console.log("Data : ",10);
+// },2000)
+// fun
+
