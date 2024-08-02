3970a1a47c97c40a84b76f65c6ebd859d76937ad
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2024-08-02 17:08:38 +0000

    Callback Hell

--- Fundamental_2.js
+++ Fundamental_2.js
@@ -13,3 +13,25 @@
 // },2000)
 // fun
 
+//02      WAITING FOR DATA
+// function getdata(data,nextdata){
+//     setTimeout(() => {
+//         console.log("Data ",data);
+//         if(nextdata){
+//            nextdata();
+//         }
+//     }, 2000);
+// }
+
+
+//03      CALLBACK HELL
+// getdata(1,()=>{
+//     console.log("Getting Data 2...");
+//     getdata(2,()=>{
+//         console.log("Getting Data 3...");
+//         getdata(3,()=>{
+//             console.log("Getting Data 4...");
+//             getdata(4)
+//         })
+//     })
+// })
